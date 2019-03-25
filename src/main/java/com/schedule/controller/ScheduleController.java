5525cda498ae4d10c35fa61120b1e26b53a23dde
7doggy <seven.doggy@gmail.com>
2019-03-25 13:25:53 +0800

    update

--- src/main/java/com/schedule/controller/ScheduleController.java
+++ src/main/java/com/schedule/controller/ScheduleController.java
@@ -18,15 +18,14 @@ public class ScheduleController {
     private Scheduler scheduler;
 
     /**
-     * 修改时间
+     * 更新job表达式
      * @param second
      * @return
      * @throws SchedulerException
      */
-    @GetMapping("/update/{second}")
+    @GetMapping("/updateJobCron/{second}")
     @ResponseBody
-    public String update(@PathVariable(name = "second") String second) throws SchedulerException {
-
+    public String updateJobCron(@PathVariable(name = "second") String second) throws SchedulerException {
         TaskDO task = taskService.get(new Long(27));
         task.setCronExpression("0/"+second+" * * * * ?");
         taskService.update(task);
@@ -36,6 +35,62 @@ public class ScheduleController {
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(task.getCronExpression());
         trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
         scheduler.rescheduleJob(triggerKey, trigger);
+        return "ok";
+    }
+
+    /**
+     * 暂停一个job
+     * @param jobId
+     * @return
+     */
+    @GetMapping("/pauseJob/{jobId}")
+    @ResponseBody
+    public String pauseJob(@PathVariable("jobId") long jobId) throws SchedulerException {
+        TaskDO task = taskService.get(jobId);
+        JobKey jobKey = JobKey.jobKey(task.getJobName(), task.getJobGroup());
+        scheduler.pauseJob(jobKey);
+        return "ok";
+    }
+
+    /**
+     * 恢复一个job
+     * @param jobId
+     * @return
+     */
+    @GetMapping("/resumeJob/{jobId}")
+    public String resumeJob(@PathVariable("jobId") long jobId) throws SchedulerException {
+        TaskDO task = taskService.get(jobId);
+        JobKey jobKey = JobKey.jobKey(task.getJobName(), task.getJobGroup());
+        scheduler.resumeJob(jobKey);
+        return "ok";
+    }
+
+    /**
+     * 删除一个job
+     * @param jobId
+     * @throws SchedulerException
+     */
+    @GetMapping("/deleteJob/{jobId}")
+    @ResponseBody
+    public String deleteJob(@PathVariable("jobId") long jobId) throws SchedulerException {
+        TaskDO task = taskService.get(jobId);
+        JobKey jobKey = JobKey.jobKey(task.getJobName(), task.getJobGroup());
+        scheduler.deleteJob(jobKey);
+        return "ok";
+    }
+
+    /**
+     * 立即触发job
+     * @param jobId
+     * @return
+     * @throws SchedulerException
+     */
+    @GetMapping("/runJobNow/{jobId}")
+    @ResponseBody
+    public String runJobNow(@PathVariable("jobId") long jobId) throws SchedulerException {
+        TaskDO task = taskService.get(jobId);
+        JobKey jobKey = JobKey.jobKey(task.getJobName(), task.getJobGroup());
+        scheduler.triggerJob(jobKey);
         return "ok";
     }
 }
